25600edec0bfba37dbbf80a8649e745eec578c7e
Stephen Ball <REBELinBLUE@users.noreply.github.com>
2016-03-15 17:53:35 +0000

    Update vagrant.sh

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -7,6 +7,7 @@ brew cask install vagrant
 # Install vagrant plugins
 vagrant plugin install vagrant-vbguest
 vagrant plugin install vagrant-notify
+vagrant plugin install vagrant-reload
 
 curl https://patch-diff.githubusercontent.com/raw/fgrehm/vagrant-notify/pull/22.patch | patch ~/.vagrant.d/gems/gems/vagrant-notify-0.4.0/lib/vagrant-notify/plugin.rb
 
