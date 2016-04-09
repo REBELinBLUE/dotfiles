bdd4e74ec765775599d8094b258803c7e7c784bf
Stephen Ball <stephen@rebelinblue.com>
2016-04-09 19:30:49 +0100

    Extra files

--- scripts/mac-apps.sh
+++ scripts/mac-apps.sh
@@ -25,6 +25,7 @@ mas install 803453959 # Slack
 mas install 416812579 # File Multi Tool 5
 mas install 924726344 # Deliveries
 mas install 870659406 # Stache
+mas install 497799835 # Xcode 7.3
 
 # mas install 409201541 # Pages
 # mas install 409183694 # Keynote
