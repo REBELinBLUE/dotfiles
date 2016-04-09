1bf7b77e4e3420b1174ff91448af0ce865f1fc43
Stephen Ball <stephen@rebelinblue.com>
2016-04-09 12:27:13 +0100

    Clean up patch command

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -11,7 +11,7 @@ vagrant plugin install vagrant-notify
 vagrant plugin install vagrant-reload
 
 # Patch vagrant notify
-curl https://patch-diff.githubusercontent.com/raw/fgrehm/vagrant-notify/pull/22.patch | patch ~/.vagrant.d/gems/gems/vagrant-notify-0.4.0/lib/vagrant-notify/plugin.rb
+curl https://patch-diff.githubusercontent.com/raw/fgrehm/vagrant-notify/pull/22.patch | patch -d ~/.vagrant.d/gems/gems/vagrant-notify-0.4.0/lib/vagrant-notify/ plugin.rb
 
 # Install homestead
 #vagrant box add --provider virtualbox laravel/homestead
