0135fc17e10d3e10e503a4a52dd900b426235aa1
Stephen Ball <stephen@rebelinblue.com>
2016-04-18 13:22:57 +0100

    Added vagrant cachier

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -6,6 +6,7 @@ source ~/.dotfiles/files/.functions
 vagrant plugin install vagrant-vbguest
 vagrant plugin install vagrant-notify
 vagrant plugin install vagrant-reload
+vagrant plugin install vagrant-cachier
 
 # Install vagrant plugins for work machine
 vagrant plugin install vagrant-bindfs
