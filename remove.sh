d0536c77bb53054131c8144b2670c354357c52b5
Stephen Ball <stephen@rebelinblue.com>
2016-04-26 22:27:54 +0100

    Nothing to remove

--- remove.sh
+++ remove.sh
@@ -2,6 +2,9 @@
 
 source $HOME/.dotfiles/scripts/utils.sh
 
+e_error "Nothing to remove"
+exit 1
+
 if ! is_os "darwin"; then
     e_error "The update script is only for OS X"
     exit 1
@@ -13,25 +16,7 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-brew remove 'ed'
-brew remove 'wdiff'
-
-# pip show apiary2postman | grep Requires | sed 's/Requires: //g; s/,//g'
-# pip show boto  | grep Requires | sed 's/Requires: //g; s/,//g'
-# pip show cheat  | grep Requires | sed 's/Requires: //g; s/,//g'
-# pip show enum34 | grep Requires | sed 's/Requires: //g; s/,//g'
-# pip show httpie  | grep Requires | sed 's/Requires: //g; s/,//g'
-# pip show httpie-jwt-auth | grep Requires | sed 's/Requires: //g; s/,//g'
-# pip show pip  | grep Requires | sed 's/Requires: //g; s/,//g'
-# pip show ply  | grep Requires | sed 's/Requires: //g; s/,//g'
-# pip show Pygments | grep Requires | sed 's/Requires: //g; s/,//g'
-# pip show setuptools  | grep Requires | sed 's/Requires: //g; s/,//g'
-# pip show vboxapi | grep Requires | sed 's/Requires: //g; s/,//g'
-# pip show wheel | grep Requires | sed 's/Requires: //g; s/,//g'
-# pip show yara-python | grep Requires | sed 's/Requires: //g; s/,//g'
-
-# Remove glances
-pip uninstall -y bottle pysnmp pysmi pyasn1 pycrypto zeroconf netifaces six python-dateutil requests pytz enum-compat influxdb statsd numpy pytz python-dateutil cycler pyparsing pystache matplotlib py-cpuinfo psutil glances
+# Commands here
 
 source $HOME/.dotfiles/scripts/dotfiles.sh
 
