bb6a0320c743a7e53411c75d83725df287bba809
Stephen Ball <stephen@rebelinblue.com>
2016-05-06 23:07:59 +0100

    Remove dockertoolbox now there is native docker

--- remove.sh
+++ remove.sh
@@ -16,7 +16,19 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Commands here
+brew cask zap dockertoolbox
+
+echo "Removing Applications..."
+sudo rm -rf /Applications/Docker
+
+echo "Removing docker binaries..."
+sudo rm -f /usr/local/bin/docker
+sudo rm -f /usr/local/bin/docker-machine
+sudo rm -r /usr/local/bin/docker-machine-driver*
+sudo rm -f /usr/local/bin/docker-compose
+
+echo "Removing boot2docker.iso"
+sudo rm -rf /usr/local/share/boot2docker
 
 source $HOME/.dotfiles/scripts/dotfiles.sh
 
