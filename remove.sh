7b3460558df714fd064b7488cc34618dd7a8068b
Stephen Ball <stephen@rebelinblue.com>
2016-05-09 09:35:00 +0100

    remove cleanup

--- remove.sh
+++ remove.sh
@@ -2,8 +2,8 @@
 
 source $HOME/.dotfiles/scripts/utils.sh
 
-# e_error "Nothing to remove"
-# exit 1
+e_error "Nothing to remove"
+exit 1
 
 if ! is_os "darwin"; then
     e_error "The update script is only for OS X"
@@ -16,19 +16,7 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-brew cask zap dockertoolbox
 
-echo "Removing Applications..."
-sudo rm -rf /Applications/Docker
-
-echo "Removing docker binaries..."
-sudo rm -f /usr/local/bin/docker
-sudo rm -f /usr/local/bin/docker-machine
-sudo rm -r /usr/local/bin/docker-machine-driver*
-sudo rm -f /usr/local/bin/docker-compose
-
-echo "Removing boot2docker.iso"
-sudo rm -rf /usr/local/share/boot2docker
 
 source $HOME/.dotfiles/scripts/dotfiles.sh
 
