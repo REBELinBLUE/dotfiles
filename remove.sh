b5a4b818805899a2661fcfc71c9cd77cadce1713
Stephen Ball <stephen@rebelinblue.com>
2016-05-12 19:52:39 +0100

    Removed apps

--- remove.sh
+++ remove.sh
@@ -13,9 +13,7 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-brew cask zap keychain
-brew cask zap goofy
-brew cask zap gitter
+
 
 source $HOME/.dotfiles/scripts/dotfiles.sh
 
