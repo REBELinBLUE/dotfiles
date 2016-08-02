acfa39e3a693c33755d1c6dd36664bdf7985db01
Stephen Ball <stephen@rebelinblue.com>
2016-08-03 00:24:02 +0100

    Removed

--- remove.sh
+++ remove.sh
@@ -13,6 +13,8 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+brew cask zap paw
+
 source $HOME/.dotfiles/scripts/dotfiles.sh
 
 exec $SHELL -l
