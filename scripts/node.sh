bcd47941dfa7aa4e2558c70a35edf9695acb3d73
Stephen Ball <stephen@rebelinblue.com>
2016-08-02 22:19:35 +0100

    Cleanup node

--- scripts/node.sh
+++ scripts/node.sh
@@ -8,4 +8,8 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+brew remove node
+sudo rm -rf /usr/local/{lib/node{,/.npm,_modules},bin,share/man}/{npm*,node*,man1/node*}
+brew install node
+
 npm install -g eslint gulp
