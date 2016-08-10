a7e1e1d0e7bb1a8530002895aa3086adb62d83d0
Stephen Ball <stephen@rebelinblue.com>
2016-08-10 13:11:38 +0100

    Added completions

---
+++ files/shell/fish/completions/itunes.fish
@@ -0,0 +1,18 @@
+complete -c itunes -f -n '__fish_use_subcommand' -a 'help' -s h -l help -d 'Print the help and exit'
+complete -c itunes -f -n '__fish_use_subcommand' -a 'launch' -d 'Launch iTunes'
+complete -c itunes -f -n '__fish_use_subcommand' -a 'quit' -d 'Quit iTunes'
+complete -c itunes -f -n '__fish_use_subcommand' -a 'play' -d 'Starts playback'
+complete -c itunes -f -n '__fish_use_subcommand' -a 'pause' -d 'Pauses playback'
+complete -c itunes -f -n '__fish_use_subcommand' -a 'next' -d 'Play the next track'
+complete -c itunes -f -n '__fish_use_subcommand' -a 'previous' -d 'Play the previous track'
+complete -c itunes -f -n '__fish_use_subcommand' -a 'pause' -d 'Pauses playback'
+complete -c itunes -f -n '__fish_use_subcommand' -a 'stop' -d 'Stop playback'
+complete -c itunes -f -n '__fish_use_subcommand' -a 'pause' -d 'Pauses playback'
+complete -c itunes -f -n '__fish_use_subcommand' -a 'mute' -d 'Mute the volume'
+complete -c itunes -f -n '__fish_use_subcommand' -a 'unmute' -d 'Umute the volume'
+complete -c itunes -f -n '__fish_use_subcommand' -a 'vol volume' -d 'Sets the volume'
+
+complete -c itunes -f -n '__fish_seen_subcommand_from vol volume' -a 'up' -d 'Increase the volume'
+complete -c itunes -f -n '__fish_seen_subcommand_from vol volume' -a 'down' -d 'Decrease the volume'
+
+
