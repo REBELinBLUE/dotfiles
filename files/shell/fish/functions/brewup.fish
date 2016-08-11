0cf0834363aeffb864e33861449e2e8b97225f9e
Stephen Ball <stephen@rebelinblue.com>
2016-08-11 13:45:08 +0100

    Fixed brewup

--- files/shell/fish/functions/brewup.fish
+++ files/shell/fish/functions/brewup.fish
@@ -3,4 +3,5 @@ function brewup -d 'Updates homebrew'
     command brew upgrade --all
     command brew bundle --file="$HOME/.dotfiles/brewfiles/all"
     command brew cleanup
+    command brew-php-switcher (cat ~/.cache/php_version) -s
 end
