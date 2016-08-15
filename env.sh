4519c276368a24c7a84bdfdee9516d6fcda24908
Stephen Ball <stephen@rebelinblue.com>
2016-08-15 23:24:47 +0100

    Added a launch service for env variables

--- env.sh
+++ env.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-launchctl setenv GNUPGHOME $HOME/.config/gnupg
+#launchctl setenv GNUPGHOME $HOME/.config/gnupg
+
+cd ~/Library/LaunchAgents
+ln -sfv ~/.dotfiles/prefs/environment.plist
+launchctl load -w environment.plist
