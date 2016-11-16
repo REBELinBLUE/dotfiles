da4fd1c2f41a293bc3e9b2c4457c5cd87db75536
Stephen Ball <stephen@rebelinblue.com>
2016-11-16 16:46:24 +0000

    Updated composer packages

--- files/shell/fish/functions/brewup.fish
+++ files/shell/fish/functions/brewup.fish
@@ -1,6 +1,6 @@
 function brewup -d 'Updates homebrew'
     command brew update
-    command brew upgrade --all
+    command brew upgrade
     command brew bundle --file="$HOME/.dotfiles/brewfiles/all"
     command brew cleanup
     command brew-php-switcher (cat ~/.cache/php_version) -s
