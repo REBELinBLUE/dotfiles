16b242cefab6a55a291b265aef57fa020a28fe48
Stephen Ball <stephen@rebelinblue.com>
2017-01-04 10:43:33 +0000

    ngrok moved to cask

--- remove.sh
+++ remove.sh
@@ -14,5 +14,6 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Remove stuff
+brew uninstall --force ngrok2
 
 source $HOME/.dotfiles/scripts/dotfiles.sh
