a965d2bc0e079d9c748947f1fd78b95018a99ab7
Stephen Ball <stephen@rebelinblue.com>
2017-02-27 00:06:33 +0000

    disable sleepnas, not working safely

--- files/shell/fish/functions/sleepnas.fish
+++ files/shell/fish/functions/sleepnas.fish
@@ -1,4 +1,4 @@
 function sleepnas -d "Sleep the NAS"
     command cat $HOME/.ssh/config.d/*.config > $HOME/.ssh/config
-    command ssh mycloud "/usr/sbin/shutdown.sh 2>&1 /dev/null"
+    #command ssh mycloud "/usr/sbin/shutdown.sh -h -P now"
 end
