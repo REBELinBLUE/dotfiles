13f95e5e7701931fede279f5f6cfcb1c532122d6
Stephen Ball <stephen@rebelinblue.com>
2017-02-27 19:17:49 +0000

    Fixed sleepnas

--- files/shell/fish/functions/sleepnas.fish
+++ files/shell/fish/functions/sleepnas.fish
@@ -1,4 +1,4 @@
 function sleepnas -d "Sleep the NAS"
     command cat $HOME/.ssh/config.d/*.config > $HOME/.ssh/config
-    #command ssh mycloud "/usr/sbin/shutdown.sh -h -P now"
+    command ssh mycloud "halt &"
 end
