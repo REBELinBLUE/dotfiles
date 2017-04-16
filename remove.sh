8cca6e8dde712c5ebdcd36aed32a8a1c197dfa05
Stephen Ball <stephen@rebelinblue.com>
2017-04-16 10:48:14 +0100

    Switched from homebrew/dupes to main repo

--- remove.sh
+++ remove.sh
@@ -15,5 +15,6 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Remove stuff
 # STUFF HERE
+brew remove homebrew/dupes/grep homebrew/dupes/diffutils homebrew/dupes/nano
 
 source $HOME/.dotfiles/scripts/dotfiles.sh
--- scripts/mac-appstore.sh
+++ scripts/mac-appstore.sh
@@ -17,22 +17,23 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # TODO: Move to mas in brewfiles
 mas install 402434720 # Name Mangler
-#mas install 1054607607 # Helium
 mas install 407963104 # Pixelmator
-#mas install 442160987 # Flycut
 mas install 467939042 # Growl
 mas install 443987910 # 1Password
 mas install 975937182 # Fantastical 2
 mas install 523620159 # CleanMyDrive 2
 mas install 411643860 # DaisyDisk
-# mas install 557168941 # Tweetbot
 mas install 588726889 # ReadKit
 mas install 402383384 # Base
 mas install 416812579 # File Multi Tool 5
 mas install 924726344 # Deliveries
 mas install 870659406 # Stache
-#mas install 497799835 # Xcode 7.3
 mas install 449589707 # Dash
+mas install 1176895641 # Spark
+mas install 1107421413 # 1blocker
+mas install 441258766 # Magnet
+mas install 603117688 # CCMenu
+mas install 1179623856 # Pastebot
 
 # mas install 409201541 # Pages
 # mas install 409183694 # Keynote
