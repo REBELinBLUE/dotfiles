9b6e82e939a4ca73a53fb0be84a2666475c62a5c
Stephen Ball <stephen@rebelinblue.com>
2017-08-13 01:00:51 +0100

    Vagrant changed the path to tee

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -27,9 +27,10 @@ if [ ! -d /etc/sudoers.d/ ]; then
     sudo mkdir /etc/sudoers.d/
 fi
 
+# The path to tee now hard coded in vagrant 1.9.7 instead of replying on $PATH
 cat > /tmp/vagrant << EOF
 # Allow Vagrant to manage NFS exports.
-Cmnd_Alias VAGRANT_EXPORTS_ADD = $(which tee) -a /etc/exports
+Cmnd_Alias VAGRANT_EXPORTS_ADD = /usr/bin/tee -a /etc/exports
 Cmnd_Alias VAGRANT_NFSD = $(which nfsd) restart
 Cmnd_Alias VAGRANT_EXPORTS_REMOVE = $(which sed) -E -e /*/ d -ibak /etc/exports
 
