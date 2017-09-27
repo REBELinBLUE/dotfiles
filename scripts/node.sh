5e3dd2d4901859a0db7d1c88796198a73db262ee
Stephen Ball <stephen@rebelinblue.com>
2017-09-27 15:18:40 +0100

    Add npm-check

--- scripts/node.sh
+++ scripts/node.sh
@@ -13,4 +13,4 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 #sudo rm -rf /uca/local/lib/node_modules
 #brew install node yarn
 
-npm install -g eslint gulp bower jslint jshint create-project jscs
+npm install -g eslint gulp bower jslint jshint create-project jscs npm-check
