7e4f7d37cbd5027b717f024881dbf1f882d3532f
Stephen Ball <stephen@rebelinblue.com>
2017-12-04 16:48:10 +0000

    Cleanup

--- scripts/others.sh
+++ scripts/others.sh
@@ -15,7 +15,8 @@ pip3 install --upgrade pip
 pip3 install httpie-oauth
 pip3 install httpie-jwt-auth
 pip3 install http-prompt
-pip3 install --user 'cozmo[camera]'
+#pip3 install --user 'cozmo[camera]'
+#pip3 install localstack
 
 # Install additional CLIs
 ln -s `which ffmpeg` /usr/local/bin/ffmpeg-static
