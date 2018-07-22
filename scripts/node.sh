4d42846eba26277a085868920023483033c2176d
Stephen Ball <stephen@rebelinblue.com>
2018-07-22 21:17:51 +0100

    Add serve command

--- scripts/node.sh
+++ scripts/node.sh
@@ -13,5 +13,5 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 #sudo rm -rf /uca/local/lib/node_modules
 #brew install node yarn
 
-npm install -g eslint gulp bower jslint jshint create-project jscs npm-check json5 nsp nanoleaves @vue/cli
+npm install -g eslint gulp bower jslint jshint create-project jscs npm-check json5 nsp nanoleaves @vue/cli serve
 npm config -g set init-license MIT
