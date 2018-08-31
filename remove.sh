d7f352354bbe25b6d1d6e3243735716ec42f46c8
Stephen Ball <stephen@rebelinblue.com>
2018-08-31 10:34:49 +0100

    Remove docker loopback

--- remove.sh
+++ remove.sh
@@ -15,5 +15,6 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Remove stuff
 rm -f /usr/local/bin/faker
+rm -f /Library/LaunchDaemons/com.docker.loopback.plist
 
 #source $HOME/.dotfiles/scripts/dotfiles.sh
