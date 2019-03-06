655888a85e9cd8a4ebd06bf31a2e1c7ee6882bd0
Stephen Ball <stephen@rebelinblue.com>
2019-03-06 19:55:21 +0000

    Remove nvm

--- scripts/node.sh
+++ scripts/node.sh
@@ -8,10 +8,10 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# brew remove node
+# brew remove node nvm
 # sudo rm -rf /usr/local/{lib/node{,/.npm,_modules},bin,share/man}/{npm*,node*,man1/node*}
 # sudo rm -rf /uca/local/lib/node_modules
-# brew install node
+# brew install node nvm
 
-npm install -g eslint create-project jscs npm-check json5 nanoleaves @vue/cli serve
+npm install -g eslint create-project jscs npm-check json5 serve
 npm config -g set init-license MIT
