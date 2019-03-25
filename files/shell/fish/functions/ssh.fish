35837c3e41442ad4c33f68a0e91b3a0fd2b9d3b5
Stephen Ball <stephen.ball@lendinvest.com>
2019-03-25 11:20:14 +0000

    Temp file

--- files/shell/fish/functions/ssh.fish
+++ files/shell/fish/functions/ssh.fish
@@ -1,7 +1,8 @@
 function ssh
     command cat $HOME/.ssh/config.d/*.config > $HOME/.ssh/config
 
-    set -l SSHCOMMAND 'sshrc'
+    ##set -l SSHCOMMAND 'sshrc' # FIXME: SEE https://github.com/Russell91/sshrc/pull/76 because gtar is no longer using default-names
+    set -l SSHCOMMAND 'command ssh'
 
     # TODO: Clean this up
     if [ $argv = "mycloud" ]
