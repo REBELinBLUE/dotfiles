7c50f9c7e548c06a25f5a5a3faf4f5219078f75e
Stephen Ball <stephen@rebelinblue.com>
2019-07-30 09:21:29 +0100

    Readded tools needed by PHPStorm

--- scripts/phputils.sh
+++ scripts/phputils.sh
@@ -47,6 +47,18 @@ chmod a+x /usr/local/bin/psysh
 mkdir -p ~/.local/share/psysh/
 curl -LsS http://psysh.org/manual/en/php_manual.sqlite -o ~/.local/share/psysh/php_manual.sqlite
 
+e_arrow 'Downloading PHP Code Sniffer'
+curl -LsS https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar -o /usr/local/bin/phpcs
+chmod a+x /usr/local/bin/phpcs
+
+e_arrow 'Downloading PHPCS Fixer'
+curl -LsS https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v2.13.1/php-cs-fixer.phar -o /usr/local/bin/php-cs-fixer
+chmod a+x /usr/local/bin/php-cs-fixer
+
+e_arrow 'Downloading PHP Mess Detector'
+curl -LsS https://github.com/phpmd/phpmd/releases/download/2.6.1/phpmd.phar -o /usr/local/bin/phpmd
+chmod a+x /usr/local/bin/phpmd
+
 rm -f /usr/local/bin/pstorm
 ln -s $HOME/.dotfiles/bin/pstorm /usr/local/bin/pstorm
 chmod +x /usr/local/bin/pstorm
