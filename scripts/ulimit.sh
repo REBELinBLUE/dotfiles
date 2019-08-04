008ca8913e1ac3550c220aee1cf2fe0a9d3122c0
Stephen Ball <stephen@rebelinblue.com>
2019-08-04 21:22:11 +0100

    Cleanup

--- scripts/ulimit.sh
+++ scripts/ulimit.sh
@@ -2,49 +2,47 @@
 
 cat << EOF > limit.maxfiles.plist
 <?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
-        "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
-  <dict>
-    <key>Label</key>
-    <string>limit.maxfiles</string>
-    <key>ProgramArguments</key>
-    <array>
-      <string>launchctl</string>
-      <string>limit</string>
-      <string>maxfiles</string>
-      <string>524288</string>
-      <string>524288</string>
-    </array>
-    <key>RunAtLoad</key>
-    <true/>
-    <key>ServiceIPC</key>
-    <false/>
-  </dict>
+    <dict>
+        <key>Label</key>
+        <string>limit.maxfiles</string>
+        <key>ProgramArguments</key>
+        <array>
+            <string>launchctl</string>
+            <string>limit</string>
+            <string>maxfiles</string>
+            <string>524288</string>
+            <string>524288</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>ServiceIPC</key>
+        <false/>
+    </dict>
 </plist>
 EOF
 
 cat << EOF > limit.maxproc.plist
 <?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
-        "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
-  <dict>
-    <key>Label</key>
-    <string>limit.maxproc</string>
-    <key>ProgramArguments</key>
-    <array>
-      <string>launchctl</string>
-      <string>limit</string>
-      <string>maxproc</string>
-      <string>2048</string>
-      <string>2048</string>
-    </array>
-    <key>RunAtLoad</key>
-    <true />
-    <key>ServiceIPC</key>
-    <false />
-  </dict>
+    <dict>
+        <key>Label</key>
+        <string>limit.maxproc</string>
+        <key>ProgramArguments</key>
+        <array>
+            <string>launchctl</string>
+            <string>limit</string>
+            <string>maxproc</string>
+            <string>2048</string>
+            <string>2048</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true />
+        <key>ServiceIPC</key>
+        <false />
+    </dict>
 </plist>
 EOF
 
