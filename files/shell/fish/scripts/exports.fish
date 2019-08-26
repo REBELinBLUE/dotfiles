323a9368a95460ae78e6c3dc90c1d81bfdb370e4
Stephen Ball <stephen@rebelinblue.com>
2019-08-26 13:59:37 +0100

    Set nvm_prefix to /usr/local/opt/nvm

--- files/shell/fish/scripts/exports.fish
+++ files/shell/fish/scripts/exports.fish
@@ -76,6 +76,7 @@ set -xg DOCKER_CONFIG $XDG_CONFIG_HOME/docker
 set -xg GRIPHOME $XDG_CONFIG_HOME/grip
 set -xg VIMINIT 'let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC'
 set -xg NVM_DIR $XDG_CONFIG_HOME/nvm
+set -xg nvm_prefix /usr/local/opt/nvm
 
 set -e RUBY_VERSION
 
---
+++ scripts/fish.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env fish
+
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
