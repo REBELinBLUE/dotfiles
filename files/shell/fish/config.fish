8bc22bb4bf87261ae1d24cfd1c8b09fbc8afc0ad
Stephen Ball <stephen@rebelinblue.com>
2019-10-08 00:12:43 +0100

    Allow PHPStorm to have fish exports

--- files/shell/fish/config.fish
+++ files/shell/fish/config.fish
@@ -3,17 +3,19 @@ if test -z "$INTELLIJ_ENVIRONMENT_READER"
 
     # Load helper files
     source $HOME/.dotfiles/files/shell/fish/scripts/helpers.fish
+end
 
-    # # Load exports
-    source $HOME/.dotfiles/files/shell/fish/scripts/exports.fish
+# Load exports
+source $HOME/.dotfiles/files/shell/fish/scripts/exports.fish
 
+if test -z "$INTELLIJ_ENVIRONMENT_READER"
     # # Load fishmarks
     source $HOME/.dotfiles/files/shell/fish/scripts/marks.fish
 
-    # # Load any additional scripts
-    if test -e $HOME/.config/extras.fish
-        source $HOME/.config/extras.fish
-    end
-
     eval (direnv hook fish)
+end
+
+# # Load any additional scripts
+if test -e $HOME/.config/extras.fish
+    source $HOME/.config/extras.fish
 end
