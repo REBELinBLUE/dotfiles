5b45046b7ae1508c52dc52fd94a298d635c95807
Stephen Ball <stephen@rebelinblue.com>
2019-10-08 00:08:29 +0100

    Stop PHPStorm from loading starship

--- files/shell/fish/config.fish
+++ files/shell/fish/config.fish
@@ -1,17 +1,19 @@
-eval (starship init fish)
+if test -z "$INTELLIJ_ENVIRONMENT_READER"
+    eval (starship init fish)
 
-# Load helper files
-source $HOME/.dotfiles/files/shell/fish/scripts/helpers.fish
+    # Load helper files
+    source $HOME/.dotfiles/files/shell/fish/scripts/helpers.fish
 
-# # Load exports
-source $HOME/.dotfiles/files/shell/fish/scripts/exports.fish
+    # # Load exports
+    source $HOME/.dotfiles/files/shell/fish/scripts/exports.fish
 
-# # Load fishmarks
-source $HOME/.dotfiles/files/shell/fish/scripts/marks.fish
+    # # Load fishmarks
+    source $HOME/.dotfiles/files/shell/fish/scripts/marks.fish
 
-# # Load any additional scripts
-if test -e $HOME/.config/extras.fish
-    source $HOME/.config/extras.fish
+    # # Load any additional scripts
+    if test -e $HOME/.config/extras.fish
+        source $HOME/.config/extras.fish
+    end
+
+    eval (direnv hook fish)
 end
-
-eval (direnv hook fish)
