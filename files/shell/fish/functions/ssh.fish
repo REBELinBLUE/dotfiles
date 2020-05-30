e4e0a56751b9fa3a28da0176238d91bbe0fe31a9
Stephen Ball <stephen@rebelinblue.com>
2020-05-30 19:19:43 +0100

    Update SSH function

--- files/shell/fish/functions/ssh.fish
+++ files/shell/fish/functions/ssh.fish
@@ -1,17 +1,7 @@
 function ssh
     command cat $HOME/.ssh/config.d/*.config > $HOME/.ssh/config
 
-    ##set -l SSHCOMMAND 'sshrc' # FIXME: SEE https://github.com/Russell91/sshrc/pull/76 because gtar is no longer using default-names
     set -l SSHCOMMAND 'command ssh'
-
-    # TODO: Clean this up
-    if [ $argv = "mycloud" ]
-        set SSHCOMMAND 'command ssh'
-    else if [ $argv = "mypassport" ]
-        set SSHCOMMAND 'command ssh'
-    else if [ $argv = "mbp" ]
-        set SSHCOMMAND 'command ssh'
-    end
 
     eval $SSHCOMMAND $argv
 end
