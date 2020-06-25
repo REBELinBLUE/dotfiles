b29cdc7e4cdec5b7f65c899f083bb26cdf764771
Stephen Ball <stephen@rebelinblue.com>
2020-06-25 23:01:03 +0100

    Remove update

--- files/shell/fish/functions/update.fish
+++
@@ -1,9 +0,0 @@
-function update -d "Updates all software"
-    sudo mas upgrade
-    sudo softwareupdate -i -a
-    command composer self-update
-    command composer global install
-    brewup
-    npm install npm -g; and npm update -g
-    #sudo pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs pip install -U -q
-end
