96428e025dfb527e7c1e6b4d6620a2bd12f2f81e
Stephen Ball <stephen@rebelinblue.com>
2020-09-24 18:16:03 +0100

    Remove quicklook plugins for now

--- remove.sh
+++ remove.sh
@@ -17,4 +17,6 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Stuff to remove on work mac when lockdown is over
 
+brew remove qlcolorcode qlmarkdown quicklook-json quicklook-csv qlstephen qlprettypatch qlimagesize
+
 #source $HOME/.dotfiles/scripts/dotfiles.sh
