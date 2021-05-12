e07c8147ac5abbd693ddd4c75e785e4339f70d56
Stephen Ball <stephen@rebelinblue.com>
2021-05-12 22:58:41 +0100

    Update dock and macapps

--- scripts/dock.sh
+++ scripts/dock.sh
@@ -12,19 +12,19 @@ brew install dockutil
 dockutil --remove all --no-restart
 dockutil --add /Applications/Safari.app --no-restart
 dockutil --add /Applications/Spark.app --no-restart
-dockutil --add /Applications/Ferdi.app --no-restart
+dockutil --add /Applications/Instapaper.app --no-restart
 dockutil --add /Applications/Transmission.app --no-restart
 dockutil --add /System/Applications/Music.app --no-restart
 dockutil --add /System/Applications/Podcasts.app --no-restart
 dockutil --add /Applications/Plex.app --no-restart
 dockutil --add /System/Applications/Reminders.app --no-restart
-dockutil --add /System/Applications/Notes.app --no-restart
+#dockutil --add /System/Applications/Notes.app --no-restart
 dockutil --add /Applications/PhpStorm.app --no-restart
-dockutil --add /Applications/DataGrip.app --no-restart
-dockutil --add /Applications/Fork.app --no-restart
-dockutil --add /Applications/Paw.app --no-restart
-dockutil --add /Applications/Patterns.app --no-restart
-dockutil --add /Applications/Sublime\ Text.app --no-restart
+#dockutil --add /Applications/DataGrip.app --no-restart
+#dockutil --add /Applications/Fork.app --no-restart
+#dockutil --add /Applications/Paw.app --no-restart
+#dockutil --add /Applications/Patterns.app --no-restart
+dockutil --add /Applications/Visual\ Studio\ Code.app --no-restart
 dockutil --add /Applications/iTerm.app --no-restart
 dockutil --add /Applications/MoneyWiz\ 3.app
 
