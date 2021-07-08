c52f4a58dc356cb78b5840a4129cca0deb43abc5
Stephen Ball <stephen.ball@lendinvest.com>
2021-07-08 07:33:22 +0100

    Do not install node, add fisher plugins

--- scripts/fish.sh
+++ scripts/fish.sh
@@ -4,4 +4,8 @@ if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
+
+    fisher install decors/fish-colored-man
+    fisher install FabioAntunes/fish-nvm
+    fisher install edc/bass
 end
