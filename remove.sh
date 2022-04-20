42bbc6a0d198d894b25dfc79be5a37b8956d11c4
Stephen Ball <stephen@rebelinblue.com>
2022-04-20 19:23:29 +0100

    clean up

--- remove.sh
+++ remove.sh
@@ -7,14 +7,8 @@ if ! is_os "darwin"; then
     exit 1
 fi
 
-# Ask for the administrator password upfront
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 # Remove stuff
 
-brew remove hopenpgp-tools mutagen-beta
+#brew remove hopenpgp-tools mutagen-beta
 
 #source $HOME/.dotfiles/scripts/dotfiles.sh
