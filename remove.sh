5d6e14145a53bddc88d5f26b961cdcefcdc417ab
Stephen Ball <stephen@rebelinblue.com>
2022-06-14 21:05:35 +0100

    remove old stuff

--- remove.sh
+++ remove.sh
@@ -3,12 +3,17 @@
 source $HOME/.dotfiles/scripts/utils/utils.sh
 
 if ! is_os "darwin"; then
-    e_error "The update script is only for OS X"
-    exit 1
+	e_error "The update script is only for OS X"
+	exit 1
 fi
 
 # Remove stuff
 
 brew remove grip
+brew remove octant
+brew remove popeye
+brew remove kubespy
+brew remove git-cal
+brew remove sshrc
 
 #source $HOME/.dotfiles/scripts/dotfiles.sh
