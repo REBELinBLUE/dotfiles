c161d20e98f6121ad50ed7dd8a6888754dc86f08
Stephen Ball <stephen.ball@lendinvest.com>
2022-06-23 15:25:52 +0100

    Change editorconfig

--- remove.sh
+++ remove.sh
@@ -3,12 +3,15 @@
 source $HOME/.dotfiles/scripts/utils/utils.sh
 
 if ! is_os "darwin"; then
-	e_error "The update script is only for OS X"
-	exit 1
+    e_error "The update script is only for OS X"
+    exit 1
 fi
 
 # Remove stuff
 
 rm /usr/local/bin/phpcs /usr/local/bin/phpcbf /usr/local/bin/php-cs-fixer /usr/local/bin/phpstan /usr/local/bin/phpunit
+
+rm -rf "${HOME}/Library/Application Support/Sublime Text/Packages/User/VcsGutter.sublime-settings"
+open "${HOME}/Library/Application Support/Sublime Text/Installed Packages"
 
 #source $HOME/.dotfiles/scripts/dotfiles.sh
