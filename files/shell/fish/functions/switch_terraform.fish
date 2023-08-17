ff8ee8fd4641b1267e4357c9ba1cb4c6afc1763f
Stephen Ball <stephen@rebelinblue.com>
2023-08-17 21:18:16 +0100

    Add tfswitch/tgswitch

---
+++ files/shell/fish/functions/switch_terraform.fish
@@ -0,0 +1,10 @@
+function switch_terraform --on-event fish_postexec
+    string match --regex '^cd\s' "$argv" > /dev/null
+    set --local is_command_cd $status
+
+    if test $is_command_cd -eq 0
+      if count .terraform-version > /dev/null
+        command tfswitch use
+      end
+    end
+end
---
+++ files/shell/fish/functions/switch_terragrunt.fish
@@ -0,0 +1,10 @@
+function switch_terragrunt --on-event fish_postexec
+    string match --regex '^cd\s' "$argv" > /dev/null
+    set --local is_command_cd $status
+
+    if test $is_command_cd -eq 0
+      if count .terragrunt-version > /dev/null
+        command tgswitch use
+      end
+    end
+end
