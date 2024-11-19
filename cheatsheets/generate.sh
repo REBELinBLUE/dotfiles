a575f62c001f2ee1a698e057f5b0291a578264c4
Stephen Ball <stephen.ball@lendinvest.com>
2024-11-19 10:40:09 +0000

    Updated

--- cheatsheets/generate.sh
+++ cheatsheets/generate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run --rm  -it --volume $PWD:/tmp --name cheatset jonasbn/cheatset:latest generate keychron.rb
+docker run --rm  -it --platform=linux/amd64 --volume $PWD:/tmp --name cheatset jonasbn/cheatset:latest generate keychron.rb
+docker run --rm  -it --platform=linux/amd64 --volume $PWD:/tmp --name cheatset jonasbn/cheatset:latest generate shortcuts.rb
---
+++ cheatsheets/shortcuts.rb
@@ -0,0 +1,51 @@
+cheatsheet do
+  title 'My Custom Keyboard Shortcuts'
+  docset_file_name 'shortcuts'
+  keyword 'shortcut'
+
+  introduction 'My Custom Key Combinations for various applications'
+
+  category do
+    id 'Global'
+
+    entry do
+      command 'cmd + space'
+      name 'Alfred'
+    end
+
+    entry do
+      command 'opt + space'
+      name 'ChatGPT'
+    end
+
+    entry do
+      command 'shift + cmd + space'
+      name '1Password'
+    end
+
+    entry do
+      command 'ctrl + opt + D'
+      name 'Dash'
+    end
+
+    entry do
+      command 'ctrl + cmd + opt + .'
+      name 'Show Hidden Search Menu Items'
+    end
+
+    entry do
+      command 'shift + cmd + opt + .'
+      name 'Quick Search Menu Bar'
+    end
+
+    entry do
+      command 'shift + cmd + 5'
+      name 'Open Snappr'
+    end
+
+    entry do
+        command 'ctrl + `'
+        name 'Moom Overlay'
+    end
+  end
+end
