15b54ef03435dc35a4b1fe01ee97d510f6cba247
Stephen Ball <stephen@rebelinblue.com>
2025-06-18 13:45:11 +0100

    Clean up

--- files/config/finicky.js
+++ files/config/finicky.js
@@ -14,48 +14,46 @@ export default {
     checkForUpdates: true,
     logRequests: false,
   },
-  rewrite: [
-    // {
-    //   match: () => true,
-    //   url: ({url}) => {
-    //       const removeKeysStartingWith = ["utm_", "uta_"]; // Remove all query parameters beginning with these strings
-    //       const removeKeys = ["fbclid", "gclid"]; // Remove all query parameters matching these keys
+  // rewrite: [
+  //   {
+  //     match: () => true,
+  //     url: ({url}) => {
+  //         const removeKeysStartingWith = ["utm_", "uta_"]; // Remove all query parameters beginning with these strings
+  //         const removeKeys = ["fbclid", "gclid"]; // Remove all query parameters matching these keys
 
-    //       const search = url.search
-    //           .split("&")
-    //           .map((parameter) => parameter.split("="))
-    //           .filter(([key]) => !removeKeysStartingWith.some((startingWith) => key.startsWith(startingWith)))
-    //           .filter(([key]) => !removeKeys.some((removeKey) => key === removeKey));
+  //         const search = url.search
+  //             .split("&")
+  //             .map((parameter) => parameter.split("="))
+  //             .filter(([key]) => !removeKeysStartingWith.some((startingWith) => key.startsWith(startingWith)))
+  //             .filter(([key]) => !removeKeys.some((removeKey) => key === removeKey));
 
-    //       return {
-    //           ...url,
-    //           search: search.map((parameter) => parameter.join("=")).join("&"),
-    //       };
-    //   },
-    // },
-  ],
+  //         return {
+  //             ...url,
+  //             search: search.map((parameter) => parameter.join("=")).join("&"),
+  //         };
+  //     },
+  //   },
+  // ],
   handlers: [
     {
-      match: (url, options) => {
-          return options.opener != null && options.opener.bundleId == "com.1password.1password"
-      },
+      match: (url, options) => options.opener?.bundleId === "com.1password.1password",
       browser: "Safari"
     },
     {
       match: (url, options) => {
-          if (options.opener != null && options.opener.bundleId == "com.bluebanana-software.inyourface") {
-              return url.host.includes("google.com")
-          }
+        if (options.opener?.bundleId == "com.bluebanana-software.inyourface") {
+            return url.host.includes("google.com")
+        }
 
-          // FIXME: Second part of this seems wrong, where did it come from? it breaks sign in with google in most apps
-          return url.host.startsWith("meet.google.com") //|| url.host.startsWith("accounts.google.com")
+        // FIXME: Second part of this seems wrong, where did it come from? it breaks sign in with google in most apps
+        return url.host.startsWith("meet.google.com") //|| url.host.startsWith("accounts.google.com")
       },
       browser: (url) => ({
-          name: "Google Chrome",
-          // args: [
-          //     '--app-id=kjgfgldnnfoeklkmfkjfagphfepbbdan',
-          //     `--app-launch-url-for-shortcuts-menu-item=${url.toString()}`,
-          // ]
+        name: "Google Chrome",
+        // args: [
+        //     '--app-id=kjgfgldnnfoeklkmfkjfagphfepbbdan',
+        //     `--app-launch-url-for-shortcuts-menu-item=${url.toString()}`,
+        // ]
       })
     },
     {
