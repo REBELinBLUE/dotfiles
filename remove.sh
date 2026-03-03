0852e354602be9a2af0540b88045d705a4aa2fc4
Stephen Ball <stephen@rebelinblue.com>
2026-03-03 18:13:07 +0000

    Cleanup

--- remove.sh
+++ remove.sh
@@ -9,4 +9,7 @@ fi
 
 # Remove stuff
 
+brew remove speedtest-cli php@8.1 pidof libass icu4c@77 libgit2@1.7
+brew untap homebrew/bundle homebrew/services homebrew/command-not-found
+ 
 #source $HOME/.dotfiles/scripts/dotfiles.sh
