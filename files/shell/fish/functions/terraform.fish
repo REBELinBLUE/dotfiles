cf7685e761e83d2ebac87742a90cd4b27badf7e7
Stephen Ball <stephen@rebelinblue.com>
2026-07-27 15:37:11 +0100

    Add wrappers for aws, terraform and terragrunt

--- files/shell/fish/functions/terraform.fish
+++ files/shell/fish/functions/terraform.fish
@@ -1,6 +1,10 @@
 function terraform
     if set -q AWS_VAULT_PROFILE; and not set -q AWS_VAULT
-        aws-vault exec $AWS_VAULT_PROFILE -- terraform $argv
+        if set -q OP_ACCOUNT; and set -q OP_ENV
+            aws-vault exec $AWS_VAULT_PROFILE -- op run --account=$OP_ACCOUNT --environment $OP_ENV -- terraform $argv
+        else
+            aws-vault exec $AWS_VAULT_PROFILE -- terraform $argv
+        end
     else
         command terraform $argv
     end
--- files/shell/fish/functions/terragrunt.fish
+++ files/shell/fish/functions/terragrunt.fish
@@ -1,6 +1,10 @@
 function terragrunt
     if set -q AWS_VAULT_PROFILE; and not set -q AWS_VAULT
-        aws-vault exec $AWS_VAULT_PROFILE -- terragrunt $argv
+        if set -q OP_ACCOUNT; and set -q OP_ENV
+            aws-vault exec $AWS_VAULT_PROFILE -- op run --account=$OP_ACCOUNT --environment $OP_ENV -- terragrunt $argv
+        else
+            aws-vault exec $AWS_VAULT_PROFILE -- terragrunt $argv
+        end
     else
         command terragrunt $argv
     end
